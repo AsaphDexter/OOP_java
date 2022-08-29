385d28f62c66fcfcc82239ad1bc3136193f70cba
AsaphDexter <36012956+AsaphDexter@users.noreply.github.com>
2022-08-29 18:11:41 +0100

    Add files via upload

---
+++ encapsulation/Student.java
@@ -0,0 +1,18 @@
+package encapsulation;
+/**
+ *
+ * @author AsaphDexter
+ */
+public class Student {
+   
+private String name;  
+   
+public String getName(){  
+return name;  
+}  
+public void setName(String name){  
+this.name = name;  
+}  
+}  
+    
+
---
+++ encapsulation/Test.java
@@ -0,0 +1,12 @@
+package encapsulation;
+/**
+ *
+ * @author AsaphDexter
+ */
+public class Test {  
+public static void main(String[] args){  
+Student myObj = new Student();  
+myObj.setName("Strawberry");  
+System.out.println(myObj.getName());  
+}  
+}  
